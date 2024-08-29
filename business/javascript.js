fbfd9634854167f0696dc474f965215ba8755780
tongducduy309 <119146803+tongducduy309@users.noreply.github.com>
2024-08-29 14:02:07 +0700

    update scanqrcode

--- business/javascript.js
+++ business/javascript.js
@@ -1,5 +1,7 @@
 import { Firestore } from "/firebase/connect-firebase.js";
-console.log(window.location.href);
+
+
+
 const fs = new Firestore({
     apiKey: "AIzaSyAXtmyMCcSb2lig6GqhFaM_0oKHHa09HWI",
     authDomain: "newapp-a6378.firebaseapp.com",
@@ -35,19 +37,24 @@ async function getIPAddress() {
     try {
       const response = await fetch('https://api.ipify.org?format=json');
       const data = await response.json();
+      console.log(data);
       return data.ip;
     } catch (error) {
       console.error('Error fetching IP address:', error);
       return 'Unknown IP';
     }
-  }
+}
 
-async function setQrcode(){
+let qrcode,urlParams;
+let user = null;
+
+function getParameterUrl(){
     const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const qrcode = urlParams.get('qrcode')
-    const doc = urlParams.get('doc')
-    //console.log(qrcode);
+    urlParams = new URLSearchParams(queryString);
+    qrcode = urlParams.get('qrcode')
+}
+
+async function addQrCode(qrcode){
     if (qrcode){
         let value = {}
         let ip = await getIPAddress();
@@ -55,11 +62,105 @@ async function setQrcode(){
             qrcode:qrcode,
             ip:ip
         }
-        console.log(value);
-        await fs.collection("business").update(doc,value)
-        //window.location.href = "about:blank";
+        await fs.collection("business").update(user,value)
+        return true;
+    }
+    return false
+}
+
+
+  
+async function setQrcode(){
+    getParameterUrl();
+    console.log(user);
+    if (user){
+        
+        if (await addQrCode(qrcode)) window.location.href = "./business.html";
+    }
+    else{
+        authen();
+        if(user)setQrcode();
+    }
+}
+const container = document.getElementById("alert_container")
+function alert_(message){
+    const alert = document.createElement("div");
+    alert.innerHTML = `<div class="alert">
+        
+        <span>${message}</span>
+      </div>`
+
+    setTimeout(()=>{
+        if(alert){
+            container.removeChild(alert)
+        }
+    },3600)
+
+    container.appendChild(alert)
+}
+
+
+
+function authen(){
+    
+    user = localStorage.getItem("business-auth");
+    console.log(user);
+    if (!user) {
+        alert_("Vui Lòng Xác Thực App")
     }
 }
 
-setQrcode()
+function onScanSuccess(decodedText, decodedResult) {
+    console.log(decodedText,);
+    if (!user&&decodedText.indexOf("cnt::")>-1){
+        const ipApp = decodedText.slice(5,decodedText.length);
+        localStorage.setItem("business-auth",ipApp)
+        alert_("Đăng Nhập Thành Công")
+        authen();
+        setQrcode();
+    }
 
+    if (user&&decodedText.indexOf("https://tongducduy.id.vn/business/business.html?qrcode=")>-1){
+        addQrCode(decodedText.replace("https://tongducduy.id.vn/business/business.html?qrcode=",""));
+
+       }
+    
+    if (isValidUrl(decodedText)) {
+      var scanResultLink = document.getElementById("scanResultUrl");
+      scanResultLink.href = decodedText;
+      scanResultLink.innerText = decodedText;
+      
+    } else {
+      document.getElementById("scanResultText").textContent = `Scan result: ${decodedText}`;
+    }
+    //html5QrcodeScanner.clear();
+
+    //document.getElementById("refreshButton").style.display = "block";
+  }
+
+  // Function to refresh the QR code scanner
+  function refreshScanner() {
+    location.reload(true);
+  }
+
+  // Function to validate URLs
+  function isValidUrl(url) {
+    var pattern = new RegExp(
+      "^(https?:\\/\\/)?" +
+        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+        "((\\d{1,3}\\.){3}\\d{1,3}))" +
+        "(\\:\\d+)?(\\/[-a-z\\d%@_.~+&:]*)*" +
+        "(\\?[;&a-z\\d%@_.,~+&:=-]*)?" +
+        "(\\#[-a-z\\d_]*)?$",
+      "i"
+    );
+    return !!pattern.test(url);
+  }
+
+  // Initialize the QR code scanner
+  var html5QrcodeScanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: 250 });
+
+  // Render the QR code scanner with the defined success callback
+  html5QrcodeScanner.render(onScanSuccess);
+
+  setQrcode();
